fb9eb49981fcc73ec8f0e99ef1f7b833ae1a7184
Oshiba Hiromitsu <oshiba@ffri.local>
2018-02-02 18:24:46 +0900

    FIX: non-essential dependency for registry

--- winenv.go
+++ winenv.go
@@ -3,10 +3,7 @@ package winenv
 
 import (
 	"fmt"
-	"os"
 	"strings"
-
-	"golang.org/x/sys/windows/registry"
 )
 
 // WindowsEnvInfo to get
@@ -17,52 +14,6 @@ type WindowsEnvInfo struct {
 	Build        string `json:"build"`
 	ServicePack  string `json:"service_pack"`
 	Architecture string `json:"architecture"`
-}
-
-// NewWindowsEnvInfo from Environment variable and Registry
-func NewWindowsEnvInfo() (envinfo WindowsEnvInfo, err error) {
-
-	k, err := registry.OpenKey(
-		registry.LOCAL_MACHINE,
-		`SOFTWARE\Microsoft\Windows NT\CurrentVersion`,
-		registry.QUERY_VALUE,
-	)
-	if err != nil {
-		return WindowsEnvInfo{}, err
-	}
-	defer k.Close()
-
-	product, _, err := k.GetStringValue("ProductName")
-	if err != nil {
-		return WindowsEnvInfo{}, err
-	}
-
-	// keys must be exists
-	version, _, err := k.GetStringValue("CurrentVersion")
-	if err != nil {
-		return WindowsEnvInfo{}, err
-	}
-
-	build, _, err := k.GetStringValue("CurrentBuildNumber")
-	if err != nil {
-		return WindowsEnvInfo{}, err
-	}
-
-	// keys maybe not exisits
-	release, _, _ := k.GetStringValue("ReleaseId")
-	servicepack, _, _ := k.GetStringValue("CSDVersion")
-
-	// Get from environment variable
-	arch := os.Getenv("Processor_Architecture")
-
-	return WindowsEnvInfo{
-		Version:      version,
-		Product:      product,
-		Release:      release,
-		Build:        build,
-		ServicePack:  servicepack,
-		Architecture: arch,
-	}, nil
 }
 
 // String for WindowsEnvInfo
